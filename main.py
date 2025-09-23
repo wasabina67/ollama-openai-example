10758f73689dceeb39d8b58a8de990c47f8065b5
wasabina67 <wasabina67@gmail.com>
2025-09-23 14:11:25 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import openai
+
+
 def main():
-    print("Hello from ollama-openai-example!")
+    client = openai.OpenAI(
+        base_url = "http://localhost:11434/v1",
+        api_key="api_key",
+    )
+
+    resp = client.chat.completions.create(
+        model="gpt-oss:20b",
+        messages=[
+            {"role": "system", "content": "あなたは優秀なアシスタントです。"},
+            {"role": "user", "content": "こんにちは。何かダジャレを言ってください。"},
+        ]
+    )
+
+    print(resp.choices[0].message.content)
 
 
 if __name__ == "__main__":
